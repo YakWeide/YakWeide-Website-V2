82bd98deda06ecefee85ca8f55b398a2b39b69a3
Tim Lisemer <timlisemer@gmail.com>
2023-03-22 14:59:28 +0100

    remove Page 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import AppHeader from './components/AppHeader/AppHeader';
+import Login from "./components/Login/Login";
+import Page1 from "./components/Main/Page1/Page1";
+import Page3 from './components/Main/Page3/Page3';
+import Discord from './components/SidePanel/Discord';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [isLoginOpen] = useState(false);
+    return (
+        <div>
+            <AppHeader />
+            <div id="site">
+                <Discord />
+                <main>
+                    <Page1 />
+                    <Page3 />
+                </main>
+            </div>
+            <Login id="Login" open={isLoginOpen}></Login>
+        </div>
+    )
 }
-
-export default App;
--- src/components/Main/Page1/Page1.js
+++ src/components/Main/Page1/Page1.js
@@ -1,14 +1,8 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import "../Main.css"
 import "./Page.css"
 
 function Page1() {
-
-    useEffect(() => {
-        // call api or anything
-        //console.log("loaded");
-        getDiscord();
-    });
 
   return (
     <section className="Wrapper" id="Page1-Wrapper">
@@ -17,42 +11,11 @@ function Page1() {
         <div>
             <p>test1</p>
 
-            <div id="info">Hoi!</div>
-            <a id="login" href="https://discord.com/api/oauth2/authorize?client_id=678012931085959194&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code&scope=identify">Identify Yourself</a>
+            <div id="info">Ho!</div>
         </div>
       </div>
     </section>
   )
-}
-
-
-function getDiscord() {
-
-    //console.log("In get Discord");
-
-    const fragment = new URLSearchParams(window.location.hash.slice(1));
-    //console.log("fragment: " + window.location.hash.toString());
-    const [accessToken, tokenType] = [fragment.get('access_token'), fragment.get('token_type')];
-
-    if (!accessToken) {
-        //console.log("!accessToken");
-        return (document.getElementById('login').style.display = 'block');
-    }
-
-    //console.log("Nach accessToken");
-
-    fetch('https://discord.com/api/users/@me', {
-        headers: {
-            authorization: `${tokenType} ${accessToken}`,
-        },
-    })
-        .then(result => result.json())
-        .then(response => {
-            const { username, discriminator } = response;
-            document.getElementById('info').innerText += ` ${username}#${discriminator}`;
-            //console.log("username " + username + " discriminator" + discriminator);
-        })
-        .catch(console.error);
 }
 
 export default Page1
