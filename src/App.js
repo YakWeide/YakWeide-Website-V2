d7cd1bc608d54a61c4b3625844717b7bbe28ebb5
Tim Lisemer <timlisemer@gmail.com>
2023-03-23 22:19:11 +0100

    New Main Panel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import AppHeader from './components/AppHeader/AppHeader';
-import Login from "./components/Login/Login";
-import Page1 from "./components/Main/Page1/Page1";
-import Page3 from './components/Main/Page3/Page3';
-import Discord from './components/SidePanel/Discord';
+import Main from "./components/Main/main";
 
 export default function App() {
-    const [isLoginOpen] = useState(false);
     return (
         <div>
             <AppHeader />
             <div id="site">
-                <Discord />
-                <main>
-                    <Page1 />
-                    <Page3 />
-                </main>
+                <Main />
             </div>
-            <Login id="Login" open={isLoginOpen}></Login>
         </div>
     )
 }
---
+++ src/components/Main/main.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './main.css';
+
+const Main = () => {
+    const [showSidePanel, setShowSidePanel] = useState(false);
+
+    const handleSidePanelToggle = () => {
+        setShowSidePanel(!showSidePanel);
+    };
+
+    return (
+        <div className="wrapper">
+            <div className="main-section" style={{ backgroundColor: 'lightblue' }}>
+                <h1>Hello World!</h1>
+                <p>This is a simple example of a colored background in the main section.</p>
+            </div>
+            <div className={`side-panel${showSidePanel ? ' expanded' : ''}`}>
+                <button className="toggle-btn" onClick={handleSidePanelToggle}>
+                    {showSidePanel ? 'Hide' : 'Show'} Panel
+                </button>
+                <iframe
+                    id="iFrame"
+                    src="https://discord.com/widget?id=288665487993339904&theme=dark"
+                    allowTransparency="true"
+                    frameBorder="0"
+                    sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Main;
