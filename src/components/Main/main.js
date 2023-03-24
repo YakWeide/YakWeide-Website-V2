bb77d1aa4ab2bd436dfc81a02582d2505f066791
Tim Lisemer <timlisemer@gmail.com>
2023-03-24 03:52:43 +0100

    New Main Panel

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -14,17 +14,19 @@ const Main = () => {
                 <h1>Hello World!</h1>
                 <p>This is a simple example of a colored background in the main section.</p>
             </div>
-            <div className={`side-panel${showSidePanel ? ' expanded' : ''}`}>
+            <div id="site-panel-control">
                 <button className="toggle-btn" onClick={handleSidePanelToggle}>
                     {showSidePanel ? 'Hide' : 'Show'} Panel
                 </button>
-                <iframe
-                    id="iFrame"
-                    src="https://discord.com/widget?id=288665487993339904&theme=dark"
-                    allowTransparency="true"
-                    frameBorder="0"
-                    sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-                />
+                <div className={`side-panel${showSidePanel ? ' expanded' : ''}`}>
+                    <iframe
+                        id="iFrame"
+                        src="https://discord.com/widget?id=288665487993339904&theme=dark"
+                        allowTransparency="true"
+                        frameBorder="0"
+                        sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+                    />
+                </div>
             </div>
         </div>
     );
